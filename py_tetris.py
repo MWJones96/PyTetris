fa77e011d74e0c1b4772bed7470de306f8114ec9
Matthew Jones <eighthmatthewjones@gmail.com>
2018-10-20 16:09:38 -0700

    Fixed collision

--- py_tetris.py
+++ py_tetris.py
@@ -17,6 +17,7 @@ class Game:
 		self.screen = screen
 		self.state = np.zeros((rows, cols), dtype=(int, 3))
 		self.current_piece = None
+		self.running = True
 		self.possible_shapes = [[[True, True, True, True]], 
 								[[True, False, False], [True, True, True]], 
 								[[False, False, True], [True, True, True]], 
@@ -41,27 +42,31 @@ class Game:
 		if not self.current_piece:
 			return
 
-		if self.current_piece.x != 0 and keys[pygame.K_LEFT]:
-			self.current_piece.x -= 1
-		elif (self.current_piece.x) != (self.cols - len(self.current_piece.shape[0])) and keys[pygame.K_RIGHT]:
-			self.current_piece.x += 1
+		if(keys[pygame.K_LEFT]):
+			 if(not self.check_collision(self.current_piece.shape, self.current_piece.x - 1, self.current_piece.y)):
+			 	self.current_piece.x -= 1
+		elif(keys[pygame.K_RIGHT]):
+			 if(not self.check_collision(self.current_piece.shape, self.current_piece.x + 1, self.current_piece.y)):
+			 	self.current_piece.x += 1
+		elif(keys[pygame.K_UP]):
+			rotated_shape = list(zip(*self.current_piece.shape[::-1]))
+			if(not self.check_collision(rotated_shape, self.current_piece.x, self.current_piece.y)):
+				self.current_piece.shape = rotated_shape
 
-		if(keys[pygame.K_UP]):
-			self.current_piece.shape = list(zip(*self.current_piece.shape[::-1]))
-
-		if(self.current_piece.y == self.rows - len(self.current_piece.shape) or self.check_collision()):
+		if(not self.check_collision(self.current_piece.shape, self.current_piece.x, self.current_piece.y + 1)):
+			self.current_piece.y += 1
+		else:
 			x_base = self.current_piece.x
 			y_base = self.current_piece.y
-
 			for row in range(len(self.current_piece.shape)):
 				for col in range(len(self.current_piece.shape[0])):
 					if(self.current_piece.shape[row][col]):
 						self.state[y_base + row][x_base + col] = self.current_piece.color
-			
 
 			self.init_piece(random.choice(self.possible_colors), random.choice(self.possible_shapes))
-		else:
-			self.current_piece.y += 1
+
+			if(self.check_collision(self.current_piece.shape, self.current_piece.x, self.current_piece.y)):
+				self.running = False
 
 	def render(self):
 		if not self.current_piece:
@@ -81,17 +86,13 @@ class Game:
 				if self.current_piece.shape[row][col]:
 					self.draw_block(x_base + col, y_base + row, self.current_piece.color)
 
-	def check_collision(self):
-		final_row = self.current_piece.shape[len(self.current_piece.shape) - 1]
-		x_base = self.current_piece.x
-		y_base = self.current_piece.y + len(self.current_piece.shape) - 1
-
-		for i in range(len(final_row)):
-			block = final_row[i]
-			x_off = x_base + i
-
-			if(block and self.state[y_base + 1][x_off].any() > 0):
-				return True
+	def check_collision(self, shape, x, y):
+		for row in range(len(shape)):
+			for col in range(len(shape[0])):
+				if(row + y > self.rows - 1 or col + x > self.cols - 1 or col + x < 0):
+					return True
+				elif(shape[row][col] and self.state[row + y][col + x].any() > 0):
+					return True
 
 		return False
 
@@ -112,8 +113,7 @@ if __name__ == "__main__":
 	g = Game(rows, cols, block_size, screen)
 	g.init_piece(random.choice(g.possible_colors), random.choice(g.possible_shapes))
 
-	running = True
-	while running:
+	while g.running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
@@ -123,6 +123,6 @@ if __name__ == "__main__":
 
 		pygame.display.update()
 		if(pygame.key.get_pressed()[pygame.K_DOWN]):
-			pygame.time.Clock().tick(16)
+			pygame.time.Clock().tick(10)
 		else:
-			pygame.time.Clock().tick(8)
+			pygame.time.Clock().tick(5)
